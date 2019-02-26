c050fdd8def0cb3177871fc677d8da52b3a77506
chintyadhita <chintyadhitaaa@gmail.com>
2019-02-26 16:49:35 +0700

    sembaran

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,8 @@ module.exports = {
  axios: {
   // baseURL: "https://still-sp/ire-77258.herokuapp.com/api/v1",
   // baseURL: "http://localhost:8000/api/v1",
-  baseURL: "http://124.158.165.11:8002/api",
+  // baseURL: "http://124.158.165.11:8002/api",
+  baseURL: "http://10.14.36.33:8000/api",
   credentials: false
  },
  auth: {
