b7fd02cf510e9bf28bb4b19b54b33b05d3d34b8a
auliya anggraini <aulia7717@gmail.com>
2019-02-28 16:24:27 +0700

    ll

---
+++ pages/report/listReport.vue
@@ -0,0 +1,66 @@
+<template>
+  <b-row>
+    <b-col lg="6">
+        <div class="animated fadeIn" style="padding: 0px">
+            <b-card style="border-radius: 3px">
+                <b-row style="width: 100%; margin-left: auto; margin-right: auto">
+                    <b-col v-for="list in lists" :key="list.id">
+                        <b-row style="padding: 2px"><strong>{{list.assignment.assignment_class}}</strong></b-row><br>
+                        <b-row style="padding: 2px">
+                            <b-col sm="4">PTL  </b-col>
+                            <b-col sm="0">:</b-col>
+                            <b-col sm="7">{{list.assignment.ptl.full_name}}</b-col>
+                        </b-row>
+                        <b-row style="padding: 2px">
+                            <b-col sm="4">Assignment Title</b-col>
+                            <b-col sm="0">:</b-col>
+                            <b-col sm="7">{{list.assignment.assignment_tittle}}</b-col>
+                        </b-row>
+                        <b-row style="padding: 2px">
+                            <b-col sm="4">Assignment Desc</b-col>
+                            <b-col sm="0">:</b-col>
+                            <b-col sm="7">{{list.assignment.assignment_desc}}</b-col>
+                        </b-row>
+                        <b-col style="padding: 10px" class="text-right">
+                            <b-button @click="showReport(list)" size="sm" variant="primary" style="margin: 10px">See More</b-button>
+                        </b-col>
+                    </b-col>
+                </b-row>
+            </b-card>
+        </div>
+    </b-col>
+  </b-row>
+</template>
+<script>
+    export default {
+    data :() => ({
+        lists: [],
+        errors: [],
+        showReports: false,
+    }),
+    mounted(){
+        this.readList();
+    },
+    methods: {
+        readList() {
+            this.$axios.get('assignment/list')
+            .then(response => {
+                this.lists = response.data;
+                console.log(this.lists);
+            })
+        },
+        showReport(list){
+            this.showReports = true;
+            this.$axios.get('/list/' + list.id)
+            .then(response => {
+                this.showReports = response.data;
+                console.log(response.data);
+                this.$router.push('/submitReport');
+            })
+            .catch(e => {
+                (error) => console.log(error)
+            });
+        },
+    }
+    }
+</script>
