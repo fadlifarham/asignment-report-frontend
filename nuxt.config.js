845864a3e9d08e8b563c74658caf703e5c77b66b
fadli7 <fadlifarham10@gmail.com>
2019-03-05 18:48:01 +0700

    after error

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,7 @@ module.exports = {
   // baseURL: "https://still-spire-77258.herokuapp.com/api/v1",
 
   // baseURL: "http://localhost:8000/api/",
-   baseURL: "http://10.14.36.47:8000/api",
+   baseURL: "http://192.168.8.101:8000/api",
   // baseURL: "http://10.14.36.33:8000/api",
   credentials: false
  },
