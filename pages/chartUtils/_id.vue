0edde386d6e8fa85e1efefc2a69bba27d61f1fc5
auliya anggraini <aulia7717@gmail.com>
2019-03-27 09:01:58 +0700

    kk

--- pages/chartUtils/_id.vue
+++ pages/chartUtils/_id.vue
@@ -1,21 +1,8 @@
 <template>
 <b-row>
-  <b-col sm="6">
-  <b-card header="Performance Member">
-    <b-row>
-      <b-col>
-        <b-row style="padding: 2px">
-                            <b-col cols="6"><strong> Name</strong></b-col>
-                            <b-col cols="0">:</b-col>
-                            <b-col sm="4">{{id}}</b-col>
-                        </b-row>
-      </b-col>
-    </b-row>
   <div class="chart-wrapper">
       <bar-example/>
     </div>
-  </b-card>
-  </b-col>
 </b-row>
 </template>
   
@@ -26,24 +13,17 @@ export default {
       components: {
         BarExample,
     },
-    validate(params) {
-      // return /^\d+$/.test(params.id)]
-      // console.log("params " + params)
-      return true
-    },
-    props: ['id'],
     data () {
         return {
-          id:'',
         }
     },
     mounted(){
-        this.getID();
+        // this.getID();
     },
     methods: {
-        getID() {
+        // getID() {
           
-        }
+        // }
     }
     
 }
