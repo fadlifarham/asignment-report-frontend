f4d2166a0c4063a97821ec50850afcd9eb2c72cb
auliya anggraini <aulia7717@gmail.com>
2019-04-05 16:57:14 +0700

    mbb

--- plugins/page.js
+++ plugins/page.js
@@ -1,11 +1,11 @@
-import Vue from 'vue'
-import VueVirtualTable from 'vue-virtual-table'
-import StarRating from 'vue-star-rating'
-import Multiselect from 'vue-multiselect'
+// import Vue from 'vue'
+// import VueVirtualTable from 'vue-virtual-table'
+// import StarRating from 'vue-star-rating'
+// import Multiselect from 'vue-multiselect'
 
-export{
-    Vue,
-    VueVirtualTable,
-    Multiselect,
-    StarRating,
-}
+// export{
+//     Vue,
+//     VueVirtualTable,
+//     Multiselect,
+//     StarRating,
+// }
